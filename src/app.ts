dbbdd7cd5b7187589170e965e4aaaaadc9e7d58b
Gabryella-Silva <gabryellaspereira7@gmail.com>
2024-09-18 16:32:29 -0300

    "atualizações"

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+//importar data.json
+//importar entities
+
+import {Product} from "./entities/product"
+import data from "../data.json"
+
+//criar produtos usando os dados do json parecido com importar os dados do codigo (post)
+
+console.log(data)
