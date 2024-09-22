183a0431a9500ad4af7214547972cd5cfbefb9d0
Gabryella-Silva <gabryellaspereira7@gmail.com>
2024-09-22 20:15:25 -0300

    atualizações que contém erros

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
-//importar data.json
-//importar entities
+// importar data.json
+// importar entities/product.ts
+// Criar os objetos de produto utilizando os
+// dados de data.json
 
-import {Product} from "./entities/product"
-import data from "../data.json"
+import { Product } from "./entities/product";
+import { shoppingCart } from "./entities/shoppingCart";
+import data from "../data.json";
 
-//criar produtos usando os dados do json parecido com importar os dados do codigo (post)
+/* interface Product {
+    name: string;  // Generalize para aceitar qualquer nome de produto
+    category: string;
+    price: number;
+    image: {
+      thumbnail: string;
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+  }
+  
 
-console.log(data)
+const products: Product[] = data;
+
+// Exemplo: Iterar pelos produtos e exibir os nomes no console
+products.forEach(product => {
+    console.log(product.name, product.price);
+}) */
+
+console.log(data[0].name);
